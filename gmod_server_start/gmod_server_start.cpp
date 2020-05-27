179069d2c1f527ce77b44a47b0b04596eaf98761
TheFUlDeep <zlobindd@mail.ru>
2020-05-27 17:51:19 +0300

    added checking online via steam and reboot if not online

--- gmod_server_start/gmod_server_start.cpp
+++ gmod_server_start/gmod_server_start.cpp
@@ -7,10 +7,20 @@
 #include <chrono>
 #include <thread>
 
-#include "HTTPRequest.hpp"
 
+#define _WIN32_WINNT 0x0A00
+#define BOOST_DATE_TIME_NO_LIB
+#define BOOST_REGEX_NO_LIB
+
+#include <boost/asio.hpp>
+
+
+#include <boost/beast.hpp>
+#include <boost/asio/connect.hpp>
+#include <boost/asio/ip/tcp.hpp>
 
 using namespace std;
+namespace http = boost::beast::http;
 
 typedef stack<string> argsstack;
 
@@ -72,7 +82,7 @@ const string GetStartArgs()
 }
 
 string port = "";
-
+string windowtitle1 = "";
 void StartServer(char** argv)
 {
     string name = argv[0];
@@ -85,7 +95,7 @@ void StartServer(char** argv)
     const string affinity = adminargsstack.top(); adminargsstack.pop();
     port = adminargsstack.top(); adminargsstack.pop();
 
-    string windowtitle1 = name + " server watchdog";
+    windowtitle1 = name + " server watchdog";
     const string start = ("@echo off\ncls\necho Protecting srcds from crashes...\ntitle " + windowtitle1 + "\n:srcds\necho (%date%) (%time%) srcds started.\n");
     const string prestartargs = (start + "cd ./server\nstart /wait /high /min /affinity " + affinity + " srcds.exe -nocrashdialog -console -tickrate 20 -autoupdate -game garrysmod -port " + port + " ");
     const string end = "\necho (%date%) (%time%) WARNING: srcds closed or crashed, restarting.\ngoto srcds";
@@ -117,31 +127,104 @@ void StartServer(char** argv)
     //-condebug
 }
 
+const string GetMyIP()
+{
+    std::ostringstream os;
+    boost::asio::ip::tcp::iostream stream("httpbin.org", "http");
+    stream << "GET /ip HTTP/1.1\r\nHost: httpbin.org\r\nConnection: close\r\n\r\n";
+    os << stream.rdbuf();
+    string str = os.str();
+
+    auto originpos = str.find("origin");
+    if (originpos != string::npos)
+    {
+        str = str.substr(originpos + 10);
+        auto secondpos = str.find('"');
+        str = str.substr(0, secondpos);
+        return str;
+    }
+    return "";
+}
+
+
 void CheckServerInOnline()
 {
-    this_thread::sleep_for(std::chrono::seconds(10));
-    const string addr = "https://api.steampowered.com/";
-    //auto response = cpr::Get(cpr::Url{ "https://api.steampowered.com/ISteamApps/GetServersAtAddress/v1/?addr=77.37.178.206:27015" });
+    this_thread::sleep_for(std::chrono::seconds(300));
 
-    try
-    {
-        // you can pass http::InternetProtocol::V6 to Request to make an IPv6 request
-        http::Request request(addr);
+    /*
+    boost::asio::ip::tcp::iostream stream("api.steampowered.com", "http");
+    stream << "GET /ip HTTP/1.1\r\nHost: api.steampowered.com\r\nConnection: close\r\n\r\n";
+    cout << stream.rdbuf();
+    */
 
-        // send a get request
-        const http::Response response = request.send("GET");
-        std::cout << std::string(response.body.begin(), response.body.end()) << '\n'; // print the result
-    }
-    catch (const std::exception& e)
+    string fulladdr = GetMyIP() + ":" + port;
+
+    const std::string host = "api.steampowered.com";
+    const std::string target = "/ISteamApps/GetServersAtAddress/v1/?addr=" + fulladdr;
+
+    // I/O контекст, необходимый для всех I/O операций
+    boost::asio::io_context ioc;
+
+    // Resolver для определения endpoint'ов
+    boost::asio::ip::tcp::resolver resolver(ioc);
+    // Tcp сокет, использующейся для соединения
+    boost::asio::ip::tcp::socket socket(ioc);
+
+    // Резолвим адрес и устанавливаем соединение
+    boost::asio::connect(socket, resolver.resolve(host, "80"));
+
+    // Дальше необходимо создать HTTP GET реквест с указанием таргета
+    http::request<http::string_body> req(http::verb::get, target, 11);
+    // Задаём поля HTTP заголовка
+    req.set(http::field::host, host);
+    req.set(http::field::user_agent, BOOST_BEAST_VERSION_STRING);
+
+    // Отправляем реквест через приконекченный сокет
+    http::write(socket, req);
+
+    // Часть, отвечающая за чтение респонса
     {
-        std::cerr << "Request failed, error: " << e.what() << '\n';
+        boost::beast::flat_buffer buffer;
+        http::response<http::dynamic_body> res;
+        http::read(socket, buffer, res);
+
+        std::ostringstream os;
+        os << res;
+        string str = os.str();
+        std::cout << str << std::endl;
+        bool online = false;
+        if (str.find(fulladdr))online = true;
+        if (!online)
+        {
+            string command = string("taskkill /F /FI ") + '"' + "WINDOWTITLE eq " + windowtitle1 + '"' + " /T";
+            cout << command << endl;
+            system(command.c_str());
+        }
     }
+    // Закрываем соединение
+    socket.shutdown(boost::asio::ip::tcp::socket::shutdown_both);
 
 }
 
 int main(int argc, char** argv)
 {
-    //StartServer(argv);
-    while (true) CheckServerInOnline();
+    bool notprogramend = true;
+    StartServer(argv);
+    //CheckServerInOnline();
+
+    thread t([&notprogramend]() 
+    {
+            while (notprogramend)
+            {
+                char s;
+                cin >> s;
+                if (s == 's') notprogramend = false;
+            }
+    });
+
+    while (notprogramend) CheckServerInOnline();
+
+    t.join();
+
     return 0;
 }
